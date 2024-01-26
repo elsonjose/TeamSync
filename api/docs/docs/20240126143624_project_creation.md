77a9027953c06e3c3ad82a79cc7c567beb11e250
elsonjose <elsonjoseofficial@gmail.com>
2024-01-26 14:51:11 +0530

    Single Project
    Moved everything to a monolithic repo

---
+++ api/docs/docs/20240126143624_project_creation.md
@@ -0,0 +1,3 @@
+## Project Creation
+
+- Following a DDD using the tutoral - https://www.youtube.com/watch?v=fhM0V2N1GpY&list=PLzYkqgWkHPKBcDIP5gzLfASkQyTdy0t4k
---
+++ api/docs/generate_file.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ $# -eq 2 ]; then
+    readme_file=$"readme.md"
+    timestamp=$(date +%Y%m%d%H%M%S)
+    file_name=docs/$timestamp'_'$1'.md'
+    if [ ! -d "docs" ]; then
+        mkdir docs
+    fi
+    echo >> $file_name && echo "File '$file_name' generated"
+    if [ ! -f $readme_file ]; then
+        echo >> $readme_file
+    fi
+    echo "- [$2]($file_name)" >> $readme_file && echo "Readme updated"
+else
+    echo "File name and short description required"
+fi
